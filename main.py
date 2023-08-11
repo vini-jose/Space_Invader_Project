8dbd43a920869bab0655adcf995a3ffa40f8d38f
vini-jose <j21990938574@gmail.com>
2023-08-10 22:54:05 -0300

    Mudança da tecla do tiro de Caps lock para o space

--- main.py
+++ main.py
@@ -3,183 +3,167 @@ import math
 import os
 from random import randint
 
-# Iniciando o pygame
-pygame.init()
-FPS  = 120
-CLOCK = pygame.time.Clock()
-
-# Initialize the pygame
+# Inicializando o pygame
 pygame.init()
 FPS = 120
 CLOCK = pygame.time.Clock()
 
-
-# Create the screen
+# Criando a tela
 SCREEN = pygame.display.set_mode((800, 600))
 
-# BACKGROUND
+# PLANO DE FUNDO
 BACKGROUND = pygame.image.load(os.path.join("imagens", "background.png"))
 
-
-# Title and Icon
+# Título e ícone
 pygame.display.set_caption("Space Invader")
 ICON = pygame.image.load(os.path.join("imagens", "ufo.png"))
 pygame.display.set_icon(ICON)
 
-# score
-score_value = 0
+# Pontuação
+valor_pontuacao = 0
 FONT = pygame.font.Font("freesansbold.ttf", 32)
 TEST_X = 10
 TEST_Y = 10
 
+def mostrar_pontuacao(x, y):
+    pontuacao = FONT.render("Pontuação: " + str(valor_pontuacao), True, (255, 255, 255))
+    SCREEN.blit(pontuacao, (x, y))
 
-def show_score(x, y):
-    score = FONT.render("Score: " + str(score_value), True, (255, 255, 255))
-    SCREEN.blit(score, (x, y))
+# Fim de jogo
+FIM_DE_JOGO_FONT = pygame.font.Font("freesansbold.ttf", 64)
 
+def texto_fim_de_jogo():
+    fim_de_jogo = FIM_DE_JOGO_FONT.render("FIM DE JOGO", True, (255, 255, 255))
+    SCREEN.blit(fim_de_jogo, (200, 250))
 
-# Game over
-GAME_OVER_FONT = pygame.font.Font("freesansbold.ttf", 64)
+# Jogador
+imagem_jogador = pygame.image.load(os.path.join("imagens", "spaceship.png"))
+posicao_jogadorX = 370
+posicao_jogadorY = 480
+mudanca_posicao_jogadorX = 0
 
-
-def game_over_text():
-    game_over = GAME_OVER_FONT.render("GAME OVER", True, (255, 255, 255))
-    SCREEN.blit(game_over, (200, 250))
-
-
-# Player
-player_img = pygame.image.load(os.path.join("imagens", "spaceship.png"))
-playerX = 370
-playerY = 480
-change_playerX = 0
-
-
-def player(x, y):
-    SCREEN.blit(player_img, (x, y))
-
+def jogador(x, y):
+    SCREEN.blit(imagem_jogador, (x, y))
 
 # Alien
-alien_img = []
-alienX = []
-alienY = []
-alienX_change = []
-alienY_change = []
-alien_down = []
-num_aliens = 6
-for i in range(num_aliens):
-    alien_img.append(pygame.image.load("imagens\\alien.png"))
-    alienX.append(randint(0, 735))
-    alienY.append(randint(50, 150))
-    alienX_change.append(1)
-    alienY_change.append(40)
-    alien_down.append(False)
+imagem_alien = []
+posicao_alienX = []
+posicao_alienY = []
+mudanca_posicao_alienX = []
+mudanca_posicao_alienY = []
+alien_destruido = []
+num_alienigenas = 6
 
+for i in range(num_alienigenas):
+    imagem_alien.append(pygame.image.load("imagens\\alien.png"))
+    posicao_alienX.append(randint(0, 735))
+    posicao_alienY.append(randint(50, 150))
+    mudanca_posicao_alienX.append(1)
+    mudanca_posicao_alienY.append(40)
+    alien_destruido.append(False)
 
-def alien(x, y):
-    SCREEN.blit(alien_img[i], (x, y))
+def alienigena(x, y):
+    SCREEN.blit(imagem_alien[i], (x, y))
 
+# Bala
+imagem_bala = pygame.image.load("imagens\\bullet.png")
+posicao_balaX = 0
+posicao_balaY = 480
+mudanca_posicao_balaX = 0
+mudanca_posicao_balaY = 5
+estado_bala = "pronta"
 
-# Bullet
-bullet_img = pygame.image.load("imagens\\bullet.png")
-bulletX = 0
-bulletY = 480
-bulletX_change = 0
-bulletY_change = 5
-bullet_state = "ready"
+def disparar_bala(x, y):
+    global estado_bala
+    estado_bala = "disparo"
+    SCREEN.blit(imagem_bala, (x + 16, y + 10))
 
-
-def fire_bullet(x, y):
-    global bullet_state
-    bullet_state = "fire"
-    SCREEN.blit(bullet_img, (x + 16, y + 10))
-
-
-def isCollision(alienX, alienY, bulletX, bulletY):
-    distance = math.sqrt(
-        (math.pow(alienX - bulletX, 2)) + (math.pow(alienY - bulletY, 2))
+def colisao(alienX, alienY, balaX, balaY):
+    distancia = math.sqrt(
+        (math.pow(alienX - balaX, 2)) + (math.pow(alienY - balaY, 2))
     )
-    if distance < 27:
+    if distancia < 27:
         return True
     return False
 
-
-# The program
-running = True
-while running:
-    # Clock
+# O programa principal
+executando = True
+while executando:
+    # Relógio
     CLOCK.tick(FPS)
 
     # RGB
     SCREEN.fill((0, 0, 0))
 
-    # BACKGROUND
+    # PLANO DE FUNDO
     SCREEN.blit(BACKGROUND, (0, 0))
 
-    # Check if not quit
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    # Verifica se não foi pedido para sair
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            executando = False
 
-    # Spawning enemies
-    for i in range(num_aliens):
-        # Game Over
-        if alienY[i] > 440:
-            for j in range(num_aliens):
-                alienY[j] = 9999
-            game_over_text()
+    # Gerando inimigos
+    for i in range(num_alienigenas):
+        # Fim de Jogo
+        if posicao_alienY[i] > 440:
+            for j in range(num_alienigenas):
+                posicao_alienY[j] = 9999
+            texto_fim_de_jogo()
             break
 
-        alienX[i] += alienX_change[i]
+        posicao_alienX[i] += mudanca_posicao_alienX[i]
 
-        if alienX[i] > 736:
-            alienX_change[i] *= -1
-            alienX[i] = 736
-            alienY[i] += alienY_change[i]
-        elif alienX[i] < 0:
-            alienX_change[i] *= -1
-            alienX[i] = 0
-            alienY[i] += alienY_change[i]
+        if posicao_alienX[i] > 736:
+            mudanca_posicao_alienX[i] *= -1
+            posicao_alienX[i] = 736
+            posicao_alienY[i] += mudanca_posicao_alienY[i]
+        elif posicao_alienX[i] < 0:
+            mudanca_posicao_alienX[i] *= -1
+            posicao_alienX[i] = 0
+            posicao_alienY[i] += mudanca_posicao_alienY[i]
 
-        # Collision
-        collision = isCollision(alienX[i], alienY[i], bulletX, bulletY)
-        if collision:
-            bulletY = 480
-            bullet_state = "ready"
-            alienX[i] = randint(0, 735)
-            alienY[i] = randint(50, 150)
-            score_value += 1
+        # Colisão
+        colisao_ocorreu = colisao(posicao_alienX[i], posicao_alienY[i], posicao_balaX, posicao_balaY)
+        if colisao_ocorreu:
+            posicao_balaY = 480
+            estado_bala = "pronta"
+            posicao_alienX[i] = randint(0, 735)
+            posicao_alienY[i] = randint(50, 150)
+            valor_pontuacao += 1
 
-        alien(alienX[i], alienY[i])
+        alienigena(posicao_alienX[i], posicao_alienY[i])
 
-    # Player
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            change_playerX = -2
-        if event.key == pygame.K_RIGHT:
-            change_playerX = 2
-        if event.key == pygame.K_CAPSLOCK and bullet_state == "ready":
-            bulletX = playerX
-            fire_bullet(bulletX, bulletY)
+    # Jogador
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        mudanca_posicao_jogadorX = -2
+    if keys[pygame.K_RIGHT]:
+        mudanca_posicao_jogadorX = 2
+    if keys[pygame.K_SPACE] and estado_bala == "pronta":
+        posicao_balaX = posicao_jogadorX
+        disparar_bala(posicao_balaX, posicao_balaY)
 
-    if event.type == pygame.K_UP:
-        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-            change_playerX = 1
-    playerX += change_playerX
+    if keys[pygame.K_UP]:
+        if keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
+            mudanca_posicao_jogadorX = 1
+    posicao_jogadorX += mudanca_posicao_jogadorX
 
-    if playerX <= 10 or playerX >= 726:
-        change_playerX *= -1
+    if posicao_jogadorX <= 10 or posicao_jogadorX >= 726:
+        mudanca_posicao_jogadorX *= -1
 
+    # Movimento da Bala
+    if posicao_balaY <= 0:
+        posicao_balaY = 480
+        estado_bala = "pronta"
 
-    # Bullet Movement
-    if bulletY <= 0:
-        bulletY = 480
-        bullet_state = "ready"
+    if estado_bala == "disparo":
+        disparar_bala(posicao_balaX, posicao_balaY)
+        posicao_balaY -= mudanca_posicao_balaY
 
-    if bullet_state == "fire":
-        fire_bullet(bulletX, bulletY)
-        bulletY -= bulletY_change
-
-    player(playerX, playerY)
-    show_score(TEST_X, TEST_Y)
+    jogador(posicao_jogadorX, posicao_jogadorY)
+    mostrar_pontuacao(TEST_X, TEST_Y)
     pygame.display.update()
 
+# Finaliza o pygame
+pygame.quit()
